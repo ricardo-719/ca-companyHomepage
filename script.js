845c8b1d7bdbcf5c6fb39992ca78e76a863fedf5
ricardo-719 <teacherrickyricardo@gmail.com>
2022-12-20 12:32:07 +0700

    Complete hero buttons Javascript animations and styling & other minor styling fixes

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const btnElOne = document.querySelector(".btnOne");
+const btnElTwo = document.querySelector(".btnTwo");
+
+btnElOne.addEventListener("mouseover", (event) =>{
+    //to collect the position from the console from pageX and pageY
+    const x = (event.pageX - btnElOne.offsetLeft);
+    const y = (event.pageY - btnElOne.offsetTop); 
+    
+    //manipulate the variales defined in style.css
+    btnElOne.style.setProperty("--xPos", x + "px");
+    btnElOne.style.setProperty("--yPos", y + "px");
+})
+
+btnElTwo.addEventListener("mouseover", (event) =>{
+    //to collect the position from the console from pageX and pageY
+    const x = (event.pageX - btnElTwo.offsetLeft);
+    const y = (event.pageY - btnElTwo.offsetTop); 
+    
+    //manipulate the variales defined in style.css
+    btnElTwo.style.setProperty("--xPos", x + "px");
+    btnElTwo.style.setProperty("--yPos", y + "px");
+})
+
+
 function active() {
     let hBN = document.getElementById('headerButtonNav');
     let hMN = document.getElementById('headerMenuNav')
