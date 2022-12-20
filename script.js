c959fe7b6a6d21884851108db0f121296d80fd19
ricardo-719 <teacherrickyricardo@gmail.com>
2022-12-20 10:30:33 +0700

    Update hamburger expand menu functionality

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function active() {
     let hBN = document.getElementById('headerButtonNav');
+    let hMN = document.getElementById('headerMenuNav')
     if (hBN.ariaPressed === "false") {
-        return hBN.ariaPressed= "true";
+        return [hBN.ariaPressed = "true", hMN.ariaPressed = "true"];
     } else {
-        return hBN.ariaPressed= "false";
+        return [hBN.ariaPressed = "false", hMN.ariaPressed = "false"];
     }
 }
